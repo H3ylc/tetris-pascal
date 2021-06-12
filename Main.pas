db87f073a6619d648c3c8e6c2cb9e92e73ec6ded
H3ylc <clyeh@pm.me>
2021-06-12 15:18:14 +0300

    Bla bla

--- Main.pas
+++ Main.pas
@@ -3,41 +3,58 @@ uses crt;
 
 const
 	FieldHeight = 20;
-	FieldWeight = 10;
-	TetriminoChar = '[]';
+	FieldWidth = 10;
+	TetriminoBlock = '[]';
 type
 	TetriminosArray = record
 		x, y: array[1..4] of integer;
+		{ Tetriminos can have only 4 blocks }
+		id: 1..7;
+		{ There is only 7 different types of tetriminos }
+		rotationPos: 1..4;
+		{ 4 different rotation positions }
 	end;
 var
-	Field: array[1..FieldWeight, 1..FieldHeight] of boolean;
-	{ True if cell is not empty }
-	PatternsOfTetriminos: array[0..7] of TetriminosArray;
+	Field: array[1..FieldWidth, 1..FieldHeight] of boolean;
+	{ True if there is something }
+	TetriminosPatterns: array[1..7] of TetriminosArray;
+	{ There is only 7 different types of tetriminos }
+
 
 procedure WriteObject(x, y: integer; color: word; obj: string);
 begin
 	GotoXY(x, y);
 	TextColor(color);
 	write(obj);
-	TextColor(White);
-	GotoXY(1,1);
+	TextColor(WHITE);
+	GotoXY(1, 1);
 end;
 
 procedure ClearObject(x, y: integer);
 begin
 	GotoXY(x, y);
 	write('  ');
-	GotoXY(1,1);
+	GotoXY(1, 1);
 end;
 
 procedure FieldInit;
-var 
+var
 	x, y: integer;
 begin
+	{ Field is empty }
+	for x := 1 to 10 do
+	begin
+		for y := 1 to 20 do
+		begin
+			Field[x, y] := false;
+		end;
+	end;
+	{ Corners }
 	WriteObject(1, 1, WHITE, '+');
-	WriteObject(1, 22, WHITE, '+');
 	WriteObject(22, 1, WHITE, '+');
+	WriteObject(1, 22, WHITE, '+');
 	WriteObject(22, 22, WHITE, '+');
+	{ Borders }
 	for x := 2 to 21 do
 	begin
 		WriteObject(x, 1, WHITE, '-');
@@ -48,108 +65,118 @@ begin
 		WriteObject(1, y, WHITE, '|');
 		WriteObject(22, y, WHITE, '|');
 	end;
-	for x := 1 to 10 do
-		for y := 1 to 20 do
-			Field[x,y] := false;
+end;
+
+{ TETRIMINOS }
+procedure WriteTetrimino(tetrim: TetriminosArray);
+var
+	i: integer;
+begin
+	for i := 1 to 4 do
+	begin
+		WriteObject(tetrim.x[i], tetrim.y[i], BLUE, TetriminoBlock);
+	end;
+end;
+
+procedure ClearTetrimino(tetrim: TetriminosArray);
+var
+	i: integer;
+begin
+	for i := 1 to 4 do
+	begin
+		ClearObject(tetrim.x[i], tetrim.y[i]);
+	end;
 end;
 
 procedure TetriminosPatternsInit;
 begin
-	{square}
-	PatternsOfTetriminos[0].x[1] := 12;
-	PatternsOfTetriminos[0].y[1] := 2;
-	PatternsOfTetriminos[0].x[2] := 12;
-	PatternsOfTetriminos[0].y[2] := 3;
-	PatternsOfTetriminos[0].x[3] := 10;
-	PatternsOfTetriminos[0].y[3] := 2;
-	PatternsOfTetriminos[0].x[4] := 10;
-	PatternsOfTetriminos[0].y[4] := 3;
+	{ 1. Square }
+	TetriminosPatterns[1].id := 1;
+	TetriminosPatterns[1].rotationPos := 1;
+	TetriminosPatterns[1].x[1] := 10;
+	TetriminosPatterns[1].y[1] := 2;
+	TetriminosPatterns[1].x[2] := 12;
+	TetriminosPatterns[1].y[2] := 2;
+	TetriminosPatterns[1].x[3] := 10;
+	TetriminosPatterns[1].y[3] := 3;
+	TetriminosPatterns[1].x[4] := 12;
+	TetriminosPatterns[1].y[4] := 3;
 end;
 
-procedure WriteTetrimino(tetr: TetriminosArray);
+
+procedure SaveInField(tetrim: TetriminosArray);
 var
-	i, j: integer; 
+	lx, ly, i: integer;
 begin
 	for i := 1 to 4 do
 	begin
-		for j := 1 to 4 do
-		begin
-			WriteObject(tetr.x[i], tetr.y[j],
-				BLUE, TetriminoChar);
-		end;
+		lx := tetrim.x[i] div 2 - 1;
+		ly := tetrim.y[i] - 1;
+		Field[lx, ly] := True;
 	end;
 end;
 
-procedure ClearTetrimino(tetr: TetriminosArray);
+function IsThereBlock(tetrim: TetriminosArray): boolean;
 var
-	i, j: integer; 
+	lx, ly, i: integer;
 begin
 	for i := 1 to 4 do
 	begin
-		for j := 1 to 4 do
+		lx := tetrim.x[i] div 2 - 1;
+		ly := tetrim.y[i];
+		if (Field[lx, ly]) or (ly = 21) then
 		begin
-			ClearObject(tetr.x[i], tetr.y[j]);
+			SaveInField(tetrim);
+			IsThereBlock := true;
+			exit;
 		end;
 	end;
+	IsThereBlock := false;
 end;
 
-procedure CreateTetrimino(var tetr: TetriminosArray);
+procedure CreateTetrimino(var tetrim: TetriminosArray);
 var
 	id: integer;
 begin
-	id := 0;
-	{id := random(7) + 1;}
-	tetr := PatternsOfTetriminos[id];
-	WriteTetrimino(tetr);
+	id := random(7) + 1;
+	id := 1;
+	tetrim := TetriminosPatterns[id];
+	WriteTetrimino(tetrim);
 end;
 
-function IsThereSomething(tetr: TetriminosArray): boolean;
+function TetriminoFall(var tetrim: TetriminosArray): boolean;
+{ If true then tetrimino fell on something }
 var
-	i, j, lx, ly: integer;
-begin
-	for i := 1 to 4 do
-	begin
-		for j := 1 to 4 do
-		begin
-			lx := tetr.x[i] div 2;
-			ly := tetr.y[j] - 1;
-			if (Field[lx, ly]) or (ly = 20) then
-			begin
-				IsThereSomething := true;
-				exit;
-			end;
-		end;
-	end;
-	IsThereSomething := false;
-end;
-
-function TetriminoFall(var tetr: TetriminosArray): boolean;
-var 
 	i: integer;
 begin
-	if IsThereSomething(tetr) then
+	if IsThereBlock(tetrim) then
 	begin
 		TetriminoFall := true;
 		exit;
 	end;
-	ClearTetrimino(tetr);
+	TetriminoFall := false;
+	ClearTetrimino(tetrim);
 	for i := 1 to 4 do
 	begin
-		tetr.y[i] := tetr.y[i] + 1;
+		tetrim.y[i] := tetrim.y[i] + 1;
 	end;
-	WriteTetrimino(tetr);
-	TetriminoFall := false;
+	WriteTetrimino(tetrim);
+end;
+{procedure TetriminoMove;
+procedure TetriminoRotate;}
+
+procedure Lose;
+begin
+	WriteObject(ScreenWidth div 2 - 4, 1, RED, 'LOSE');
+	delay(1000);
+	clrscr();
+	halt();
 end;
 
-{
-procedure MoveBlocksDown;
-procedure RotateTetrimino;
-procedure DeleteLineOfTetriminos;
-}
-
 var
-	x, y: integer;
 	CurrentTetrimino: TetriminosArray;
+	ch: char;
+	i: integer;
 begin
 	randomize();
 	clrscr();
@@ -158,9 +185,22 @@ begin
 	CreateTetrimino(CurrentTetrimino);
 	while true do
 	begin
-		delay(250);
+		delay(100);
+		for i := 4 to 8 do
+		begin
+			if (Field[i, 2]) or (Field[i, 1]) then
+				Lose;
+		end;
 		if TetriminoFall(CurrentTetrimino) then
-			break;
+		begin
+			CreateTetrimino(CurrentTetrimino);
+		end;
+		ch := readkey();
+		if ch = 'q' then
+		begin
+			clrscr();
+			halt;
+		end;
 	end;
 	readkey();
 	clrscr();
